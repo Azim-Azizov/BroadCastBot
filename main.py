d4b75cd254629f5fcae7ebc5c83df64b7d23fff0
SV Developer <87415292+Azim-Azizov@users.noreply.github.com>
2022-12-17 13:35:57 +0400

    Initial commit

--- main.py
+++ main.py
@@ -5,18 +5,15 @@ from pyrogram.errors import BadRequest, FloodWait
 
 from pymongo import MongoClient
 
+from config import *
 
-API_ID = 5435275
-API_HASH = "3b3b4f97a8795e97863f6843dcc1c199"
-TOKEN = "5366100656:AAFk8pzxYCK-ot_L5AMHFUMY-MFCKM5GdrU"
-DB_URL = "mongodb://sudo:12345678@localhost:27017/?authMechanism=DEFAULT"
-USERNAME = "CrazyGameAzBot"
-SUDO = [881143712, 1407140052]
 
 sent, fail, total = 0, 0, 0
 
 
-coll = MongoClient(DB_URL).namegame.user
+db = MongoClient(DB_URL).get_database(DB_NAME)
+user_coll = db.get_collection(USER_COLL_NAME)
+chat_coll = db.get_collection(CHAT_COLL_NAME)
 
 app = Client(USERNAME, API_ID, API_HASH, bot_token=TOKEN)
 
@@ -27,28 +24,30 @@ async def broadcast(app, message):
     if user_id not in SUDO:
         return
     if not message.reply_to_message:
-        return await message.reply("Mesaja cavab ver.")
+        return await message.reply("Reply to a message to broadcast it.")
     msg = message.reply_to_message
     global sent, fail, total
     sent = 0
     fail = 0
     total = 0
     data = []
-    dev = await message.reply(f"yayın başlayır...")
+    dev = await message.reply(f"Broadcast is starting...")
     if "-u" in message.text:
-        for d in coll.find({"_id": {"$gt": 0}}):
+        for d in user_coll.find({ID_FIELD_NAME: {"$gt": 0}}):
             data.append(d)
     elif "-c" in message.text:
-        for d in coll.find({"_id": {"$lt": 0}}):
+        for d in chat_coll.find({ID_FIELD_NAME: {"$lt": 0}}):
             data.append(d)
     else:
-        for d in coll.find({}):
+        for d in user_coll.find({ID_FIELD_NAME: {"$gt": 0}}):
+            data.append(d)
+        for d in chat_coll.find({ID_FIELD_NAME: {"$lt": 0}}):
             data.append(d)
     for d in data:
         State = False
         while not State:
             try:
-                await msg.forward(d["_id"])
+                await msg.forward(d[ID_FIELD_NAME])
                 sent += 1
                 State = True
                 await asyncio.sleep(0.125)
@@ -65,13 +64,13 @@ async def broadcast(app, message):
         total += 1
         if not total % 50:
             try:
-                await dev.edit_text(f"Yayın davam edir...\n\nMesaj {total} çata göndərilib. Bunlardan {sent} uğurlu, {fail} uğursuz nəticə əldə olunub.")
+                await dev.edit_text(f"Broadcast is continuing...\n\nmessage sent to {total} chats. From those {sent} were successful,  and {fail} were failed.")
                 await asyncio.sleep(1)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
     await asyncio.sleep(3)
     await dev.delete()
-    await message.reply(f"Yayın sona çatdı.\n\nMesaj {total} çata göndərilib. Bunlardan {sent} uğurlu, {fail} uğursuz nəticə əldə olunub.")
+    await message.reply(f"Broadcast ended.\n\nmessage sent to {total} chats. From those {sent} were successful,  and {fail} were failed.")
 
 app.start()
 idle()
