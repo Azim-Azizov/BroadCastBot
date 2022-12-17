e113547a6982f074aeedff9407973dc68ac31e80
SV Developer <87415292+Azim-Azizov@users.noreply.github.com>
2022-12-17 15:46:28 +0400

    Update form

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 # BroadCastBot
  
 This bot will help you to broadcast message using telegram bot, and MongoDB database.
+In addition, you can stop broadcasting whenever you want. Just click particular button below the status message.
 
 # Initialization
 
 Just edit sample_config.py with your variables, and rename it to config.py
 Run command python{your version} main.py
 
-#Usage
+# Usage
 
 /gcast -u | To broadcast only to users.
 /gcast -c | To broadcast only to chats.
 /gcast | To broadcast to everyone.
+
+# ATTENTION!!!
+
+You can broadcast only one message at once!
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from pyrogram import Client, filters, idle
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import BadRequest, FloodWait
 
 from pymongo import MongoClient
@@ -8,11 +9,25 @@ from pymongo import MongoClient
 from config import *
 
 
+STOP = InlineKeyboardMarkup([[InlineKeyboardButton(text="Stop", callback_data="STOP_BROADCAST")]])
+
+total, sent, fail, stop = 0, 0, 0, False
+
 db = MongoClient(DB_URL).get_database(DB_NAME)
 user_coll = db.get_collection(USER_COLL_NAME)
 chat_coll = db.get_collection(CHAT_COLL_NAME)
 
 app = Client(USERNAME, API_ID, API_HASH, bot_token=TOKEN)
+
+
+@app.on_callback_query(filters.regex("STOP_BROADCAST"))
+async def stop_broadcast(app, CallbackQuery):
+    user_id = CallbackQuery.from_user.id
+    if user_id not in SUDO:
+        return
+    global stop
+    stop = True
+    await CallbackQuery.answer("Broadcast stopped.", show_alert = True)
 
 
 @app.on_message(filters.command(["gcast"]))
@@ -23,11 +38,16 @@ async def broadcast(app, message):
     if not message.reply_to_message:
         return await message.reply("Reply to a message to broadcast it.")
     msg = message.reply_to_message
+    global total, sent, fail, stop
+    if total:
+        return await message.reply("Eyni vaxtda yalnız bir yayın etmək olar.")
     sent = 0
     fail = 0
     total = 0
+    stop = False
     data = []
-    dev = await message.reply(f"Broadcast is starting...")
+    dev = await message.reply(f"Broadcast is starting...", reply_markup = STOP)
+    await asyncio.sleep(10)
     if "-u" in message.text:
         for d in user_coll.find({ID_FIELD_NAME: {"$gt": 0}}):
             data.append(d)
@@ -40,13 +60,14 @@ async def broadcast(app, message):
         for d in chat_coll.find({ID_FIELD_NAME: {"$lt": 0}}):
             data.append(d)
     for d in data:
+        if stop:
+            break
         State = False
         while not State:
             try:
                 await msg.forward(d[ID_FIELD_NAME])
                 sent += 1
                 State = True
-                await asyncio.sleep(0.125)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
             except BadRequest:
@@ -56,17 +77,21 @@ async def broadcast(app, message):
                 print(e)
                 fail += 1
                 State = True
-                await asyncio.sleep(0.3)
         total += 1
-        if not total % 50:
+        await asyncio.sleep(0.05)
+        if not total % 60:
             try:
-                await dev.edit_text(f"Broadcast is continuing...\n\nmessage sent to {total} chats. From those {sent} were successful,  and {fail} were failed.")
-                await asyncio.sleep(1)
+                await dev.edit_text(f"Broadcast is continuing...\n\nmessage sent to {total} chats. From those {sent} were successful,  and {fail} were failed.", reply_markup = STOP)
+                await asyncio.sleep(0.05)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
-    await asyncio.sleep(3)
+                await dev.edit_text(f"Broadcast is continuing...\n\nmessage sent to {total} chats. From those {sent} were successful,  and {fail} were failed.", reply_markup = STOP)
     await dev.delete()
-    await message.reply(f"Broadcast ended.\n\nmessage sent to {total} chats. From those {sent} were successful,  and {fail} were failed.")
+    await asyncio.sleep(3)
+    if stop:
+        await message.reply(f"Broadcast ended.\n\nmessage sent to {total} chats. From those {sent} were successful,  and {fail} were failed.", reply_markup=None)
+    else:
+        await message.reply(f"Broadcast ended.\n\nmessage sent to {total} chats. From those {sent} were successful,  and {fail} were failed.", reply_markup=None)
 
 app.start()
 idle()
